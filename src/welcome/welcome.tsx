b0e2c882aa1b1f961cc39e0aff50edf8f452300d
Taeyoung Lee <ty91kr@gmail.com>
2025-09-18 22:21:18 +0900

    test

--- src/welcome/welcome.tsx
+++ src/welcome/welcome.tsx
@@ -19,7 +19,9 @@ export function Welcome() {
         </header>
         <div className="max-w-[320px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">Resources</p>
+            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+              Helpful resources
+            </p>
             <ul className="space-y-2">
               {resources.map(({ href, label, description }) => (
                 <li key={href}>
